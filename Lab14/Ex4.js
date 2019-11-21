b7e6c2b43204ecbfcff74b5c9cc1df21c53459f1
Kobe Dait <kobedait@hawaii.edu>
2019-11-21 11:17:05 -1000

    Edits to Lab 14

--- Lab14/Ex4.js
+++ Lab14/Ex4.js
@@ -10,15 +10,16 @@ if (fs.existsSync(filename)) {
     data = fs.readFileSync(filename, 'utf-8');
 
     stats = fs.statSync(filename)
-    console.log(filename + ' has ' + stats.size + ' characters');
+    console.log(filename + ' has ' + stats.size + ' characters'); //Possible way to write identifiers
 
     users_reg_data = JSON.parse(data); // Takes a string and converts it into object or array
 
-    console.log(users_reg_data.itm352.password);
+    console.log(users_reg_data.itm352.password); // Possible way to write identifiers
 } else {
     console.log(filename + ' does not exist!');
 }
 
+// CHANGE /login to HTML
 app.get("/login", function (request, response) {
     // Give a simple login form
     str = `
@@ -33,6 +34,7 @@ app.get("/login", function (request, response) {
     response.send(str);
 });
 
+// CHANGE /login to HTML
 app.post("/login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
     console.log(request.body);
@@ -40,9 +42,9 @@ app.post("/login", function (request, response) {
     the_username = request.body.username;
     if (typeof users_reg_data[the_username] != 'undefined') {
         if (users_reg_data[the_username].password == request.body.password) {
-            response.send(the_username + ' logged in!');
+            response.send(the_username + ' logged in!'); // Change to redirect to invoice html
         } else {
-            response.redirect('/login');
+            response.redirect('/login'); // Write an alert to notify
         }
     }
 });
